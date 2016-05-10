88b5898a8b7cdcf959dc8cee8a2fff69b2e64dab
Dominik Plewa <plewad@gmail.com>
2016-05-10 22:12:04 +0200

    Handle code passed from args

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import System.Environment
 import System.IO
 
 import ParHaskal
@@ -14,9 +15,12 @@ import Utils
 {- Unfortunately, output will be printed only if no runtime errors occur. -}
 
 main = do
-    code <- getContents
+    args <- getArgs
+    code <- if null args
+        then getContents
+        else readFile $ head args
     case pBlock (myLexer code) of
         Ok b    -> case runInterpreter (run b emptyEnv) emptyStore of
-    	               Left msg        -> hPutStrLn stderr ("Runtime error: " ++ msg)
-    	               Right (outs, _) -> mapM_ putStrLn outs
-    	Bad err -> hPutStrLn stderr ("Parser error: " ++ err)
+                       Left msg        -> hPutStrLn stderr ("Runtime error: " ++ msg)
+                       Right (outs, _) -> mapM_ putStrLn outs
+        Bad err -> hPutStrLn stderr ("Parser error: " ++ err)
